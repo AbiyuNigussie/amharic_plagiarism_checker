8dcaf3b430efe72bdc40d8466cbbe332ef84fd90
AbiyuTolosa <abiyunigussie7@gmail.com>
2025-01-04 17:12:56 +0300

    main.py modified, test_docs files deleted.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from preprocess import preprocess_text
-import pandas as pd
 import os
+import pandas as pd
+from preprocess import preprocess_text
 from feature_extraction import tfidf_features
 from similarity_cosine import calc_cosine_similarity
 
@@ -8,26 +8,24 @@ from similarity_cosine import calc_cosine_similarity
 df = pd.read_csv("stopwords.csv")
 amharic_stopwords = df["stopword"].tolist()
 
-file_names = [file for file in os.listdir("./test_docs") if file.endswith(".txt")]
-docs = [open(f"./test_docs/{file}", encoding="utf-8").read() for file in file_names]
 
-preprocessed_docs = [preprocess_text(doc, amharic_stopwords) for doc in docs]
+def check_plagiarism(file_paths):
 
-doc_vec = tfidf_features(preprocessed_docs)
-doc_filename_pairs = list(zip(file_names, doc_vec))
-
-
-def check_plagiarism():
+    print(file_paths)
+    docs = [open(file, encoding="utf-8").read() for file in file_paths]
+    preprocessed_docs = [preprocess_text(doc, amharic_stopwords) for doc in docs]
+    print(preprocessed_docs)
+    # Generate TF-IDF vectors
+    doc_vec = tfidf_features(preprocessed_docs)
+    doc_filename_pairs = list(
+        zip([os.path.basename(file) for file in file_paths], doc_vec)
+    )
 
     plagiarism_results = set()
 
-    global doc_filename_pairs
-
     for a_file, a_vec in doc_filename_pairs:
-
         remaining_pairs = doc_filename_pairs.copy()
         current_index = remaining_pairs.index((a_file, a_vec))
-
         del remaining_pairs[current_index]
 
         for b_file, b_vec in remaining_pairs:
@@ -37,14 +35,7 @@ def check_plagiarism():
             plagiarism_result = (
                 sorted_filenames[0],
                 sorted_filenames[1],
-                similarity_score,
+                round(similarity_score * 100, 2),
             )
             plagiarism_results.add(plagiarism_result)
-
     return plagiarism_results
-
-
-results = check_pliagrism()
-
-for x in results:
-    print(x)
